71916fd08603858835602d9738c222a52c5e1608
RagdeGFC <ragde.gfc@gmail.com>
2025-02-18 02:58:46 -0800

    Eliminar carpeta dist del repositorio

--- dist/config/regular-exp.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.regularExp = void 0;
-exports.regularExp = {
-    email: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/,
-    password: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*\W)(?=.*[a-zA-Z]).{10,16}$/,
-};
